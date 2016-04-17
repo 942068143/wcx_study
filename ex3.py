30cbd9dd29867d4f8f14366759e90ef9ac473b3b
wangcaixia11 <wangcaixia@jd.com>
2016-04-17 19:14:13 +0800

    四则运算练习

---
+++ ex3.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# 四则运算练习
+from __future__ import division # python2中两整数相除结果是浮点型
+print('四则运算')
+print 25 + 30/6
+print 25 + 30/5
+print 4%2
+print 4-3
+print 4*2
+print 1/3
+print 1.0/3
+print 3+2<5-7
+print 5>-2
+
+
+
+
+
+
+
+
+
+
